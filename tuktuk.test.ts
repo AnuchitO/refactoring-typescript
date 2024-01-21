3ff31c0f093919d8f3322ff81e177d66c89c5f26
Nong AnuchitO <anuchito.code@gmail.com>
2024-01-21 17:08:04 +0700

    add tuktuk

---
+++ tuktuk.test.ts
@@ -0,0 +1,7 @@
+import tuktuk from './tuktuk'
+
+describe('demo Tuk Tuk', () => {
+  test('tuktuk 1 + 2 to equal 3', () => {
+    expect(tuktuk(1, 2)).toBe(3)
+  })
+})
---
+++ tuktuk.ts
@@ -0,0 +1,5 @@
+function tuktuk(a: number, b: number): number {
+  return a + b
+}
+
+export default tuktuk
